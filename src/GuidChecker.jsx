366395b0c6379d8e355bcfcbf875fa26d1f19cbd
Marton Balassa <7115274+BalassaMarton@users.noreply.github.com>
2023-08-18 16:42:50 +0200

    Added spinner

--- src/GuidChecker.jsx
+++ src/GuidChecker.jsx
@@ -5,17 +5,25 @@ const GuidChecker = () => {
   const [isUnique, setIsUnique] = useState(false);
   const [isValidGuid, setIsValidGuid] = useState(true);
   const [guidInput, setGuidInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // State for spinner
 
   const checkGuidUniqueness = () => {
-    const isValid = uuidValidate(guidInput);
-    
-    if (isValid) {
-      setIsValidGuid(true);
-      setIsUnique(true);
-    } else {
-      setIsValidGuid(false);
-      setIsUnique(false);
-    }
+    setIsLoading(true); // Display spinner
+
+    // Simulate a long-running operation using setTimeout
+    setTimeout(() => {
+      const isValid = uuidValidate(guidInput);
+
+      setIsLoading(false); // Hide spinner
+
+      if (isValid) {
+        setIsValidGuid(true);
+        setIsUnique(true);
+      } else {
+        setIsValidGuid(false);
+        setIsUnique(false);
+      }
+    }, 3000); // Simulate 3 seconds delay
   };
 
   return (
@@ -27,19 +35,24 @@ const GuidChecker = () => {
         value={guidInput}
         onChange={(e) => setGuidInput(e.target.value)}
       />
-      <button onClick={checkGuidUniqueness}>Check</button>
-      {!isValidGuid && (
-        <div className="result invalid">
-          <span className="cross-mark">✕</span>
-          <p>This is not a valid GUID</p>
-        </div>
-      )}
-      {isUnique && isValidGuid && (
-        <div className="result">
-          <span className="check-mark">✓</span>
-          <p>Yes, your GUID is unique</p>
-        </div>
-      )}
+      <button onClick={checkGuidUniqueness} disabled={isLoading}>
+        Check
+      </button>
+      <div className="spinner-section">
+        {isLoading && <div className="spinner"></div>}
+        {!isValidGuid && (
+          <div className={`result ${isValidGuid ? '' : 'invalid'}`}>
+            <span className="icon">{isValidGuid ? '✓' : '✕'}</span>
+            <p>{isValidGuid ? 'Yes, your GUID is unique' : 'This is not a valid GUID'}</p>
+          </div>
+        )}
+        {isUnique && isValidGuid && (
+          <div className="result">
+            <span className="icon">✓</span>
+            <p>Yes, your GUID is unique</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
