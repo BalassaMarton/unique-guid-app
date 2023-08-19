c5b04e2e452bad228f7a130a90c3d7f30ec31d71
Marton Balassa <7115274+BalassaMarton@users.noreply.github.com>
2023-08-19 11:20:06 +0200

    Add github logo and link

--- src/GuidChecker.jsx
+++ src/GuidChecker.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
+import githubLogo from './github-logo.png'; // Import your GitHub logo image
+import './GuidChecker.css'; // Import your CSS file
 
 const GuidChecker = () => {
   const [isUnique, setIsUnique] = useState(false);
@@ -34,6 +36,11 @@ const GuidChecker = () => {
 
   return (
     <div className="guid-checker">
+      <div className="github-banner">
+        <a href="https://github.com/BalassaMarton/unique-guid-app" target="_blank" rel="noopener noreferrer">
+          <img src={githubLogo} alt="GitHub Logo" />
+        </a>
+      </div>
       <label>Is my GUID unique?</label>
       <input
         type="text"
@@ -47,19 +54,19 @@ const GuidChecker = () => {
       </button>
       <div className="spinner-section">
         {isLoading && <div className="spinner"></div>}
-        {!isValidGuid && (
-          <div className={`result ${isValidGuid ? '' : 'invalid'}`}>
-            <span className="icon">{isValidGuid ? '✓' : '✕'}</span>
-            <p>{isValidGuid ? 'Yes, your GUID is unique' : 'This is not a valid GUID'}</p>
-          </div>
-        )}
-        {isUnique && isValidGuid && (
-          <div className="result">
-            <span className="icon">✓</span>
-            <p>Yes, your GUID is unique</p>
-          </div>
-        )}
       </div>
+      {!isValidGuid && (
+        <div className={`result ${isValidGuid ? '' : 'invalid'}`}>
+          <span className="icon">{isValidGuid ? '✓' : '✕'}</span>
+          <p>{isValidGuid ? 'Yes, your GUID is unique' : 'This is not a valid GUID'}</p>
+        </div>
+      )}
+      {isUnique && isValidGuid && (
+        <div className="result">
+          <span className="icon">✓</span>
+          <p>Yes, your GUID is unique</p>
+        </div>
+      )}
     </div>
   );
 };
